dfd28481de76bc2d7aa32ad00e5f20d4e4472722
Đặng Tấn <tandang264@gmail.com>
2023-05-11 23:13:04 +0700

    Add files via upload

---
+++ src/PuzzleGame.py
@@ -0,0 +1,341 @@
+import pygame, sys, random
+from pygame.locals import *
+import threading
+from a_star import guide
+from tkinter import messagebox
+
+# Khai báo các hằng số
+BOARDWIDTH = 3  
+BOARDHEIGHT = 3 
+TILESIZE = 80 # Kích thước mỗi ô
+WINDOWWIDTH = 640 # Độ rộng cửa sổ game
+WINDOWHEIGHT = 480 # Độ cao cửa sổ game
+FPS = 30
+BLANK = 0 # Tượng trưng cho ô trống(0)
+LEVEL = 80 # Số lần tráo. VD: ở đây sẽ tráo 80 lần.
+
+# Thiết đặt các màu sẽ dùng
+#                 R    G    B
+BLACK =         (  0,   0,   0)
+WHITE =         (255, 255, 255)
+BRIGHTBLUE =    (  0,  50, 255)
+DARKTURQUOISE = (  3,  54,  73)
+GREEN =         (  0, 204,   0)
+
+# Thiết đặt màu cho các đối tượng sẽ hiển thị
+BGCOLOR = DARKTURQUOISE
+TILECOLOR = GREEN
+TEXTCOLOR = WHITE
+BORDERCOLOR = BRIGHTBLUE
+BASICFONTSIZE = 20
+
+BUTTONCOLOR = WHITE
+BUTTONTEXTCOLOR = BLACK
+MESSAGECOLOR = WHITE
+
+# Toạ độ của bảng game
+XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT, GUIDE_SURF, GUIDE_RECT, ABOUT_SURF, ABOUT_RECT
+
+    # Thiết đặt các đối tượng trên giao diện
+    pygame.init()
+    img = pygame.image.load('res/icon.png')
+    pygame.display.set_icon(img)
+
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Slide Puzzle')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+
+    
+    GUIDE_SURF, GUIDE_RECT = makeText('Guide',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 150)
+    RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 120)
+    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
+    SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+    ABOUT_SURF, ABOUT_RECT = makeText('About', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
+
+    mainBoard, solutionSeq = generateNewPuzzle(LEVEL)
+    SOLVEDBOARD = getStartingBoard() # bảng đích
+    allMoves = [] # lưu danh sách các bước di chuyển mà người dùng thực hiện
+
+    while True: # vòng lặp chính của trò chơi
+        slideTo = None # hướng di chuyển
+        msg = 'Click tile or press arrow keys to slide.' # đoạn text sẽ xuất hiện phía trái trên cùng của màn hình
+        if mainBoard == SOLVEDBOARD:
+            msg = 'Solved!'
+            solutionSeq = []
+            allMoves = []
+
+        drawBoard(mainBoard, msg)
+
+        checkForQuit()
+        for event in pygame.event.get(): # bắt các sự kiện gây ra từ người dùng
+            if event.type == MOUSEBUTTONUP: # nếu người dùng dùng chuột click
+                spotx, spoty = getSpotClicked(mainBoard, event.pos[0], event.pos[1]) # bắt toạ độ người dùng click
+
+                if (spotx, spoty) == (None, None): # nếu toạ độ không nằm trong các ô thì có thể là nhấn vào các text bên phải màn hình
+                    if RESET_RECT.collidepoint(event.pos):
+                        if (mainBoard == SOLVEDBOARD):
+                            continue
+                        resetAnimation(mainBoard, allMoves) # nhấn vào nút Reset
+                        allMoves = []
+                    elif NEW_RECT.collidepoint(event.pos):
+                        mainBoard, solutionSeq = generateNewPuzzle(LEVEL) # nhấn vào nút New game
+                        allMoves = []
+                    elif SOLVE_RECT.collidepoint(event.pos):
+                        if (mainBoard == SOLVEDBOARD):
+                            continue
+                        resetAnimation(mainBoard, solutionSeq + allMoves) # nhấn vào nút Solve
+                        allMoves = []
+                    elif GUIDE_RECT.collidepoint(event.pos): # nhấn vào nút Guide
+                        if (mainBoard == SOLVEDBOARD):
+                            continue
+                        x = threading.Thread(target=guide, args=(mainBoard,))
+                        x.start()
+                    elif ABOUT_RECT.collidepoint(event.pos): # nhấn vào nút About           
+                        showAboutDialog()                     
+                else: # nếu toạ độ là các ô
+                    blanky, blankx = getBlankPosition(mainBoard)
+                    if spotx == blankx + 1 and spoty == blanky:
+                        slideTo = LEFT
+                    elif spotx == blankx - 1 and spoty == blanky:
+                        slideTo = RIGHT
+                    elif spotx == blankx and spoty == blanky + 1:
+                        slideTo = UP
+                    elif spotx == blankx and spoty == blanky - 1:
+                        slideTo = DOWN
+
+            elif event.type == KEYUP: # nếu người dùng dùng phím điều hướng
+                if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
+                    slideTo = LEFT
+                elif event.key in (K_RIGHT, K_d) and isValidMove(mainBoard, RIGHT):
+                    slideTo = RIGHT
+                elif event.key in (K_UP, K_w) and isValidMove(mainBoard, UP):
+                    slideTo = UP
+                elif event.key in (K_DOWN, K_s) and isValidMove(mainBoard, DOWN):
+                    slideTo = DOWN
+
+        if slideTo:
+            slideAnimation(mainBoard, slideTo, 'Click tile or press arrow keys to slide.', 8) # hiển thị animation
+            makeMove(mainBoard, slideTo)
+            allMoves.append(slideTo) # thêm cách thức di chuyển vào allMoves
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT): # get all the QUIT events
+        terminate() # terminate if any QUIT events are present
+    for event in pygame.event.get(KEYUP): # get all the KEYUP events
+        if event.key == K_ESCAPE:
+            terminate() # terminate if the KEYUP event was for the Esc key
+        pygame.event.post(event) # put the other KEYUP event objects back
+
+
+def getStartingBoard(): # sinh ma trận ban đầu(=ma trận đích)
+    counter = 1
+    board = []
+    for row in range(BOARDHEIGHT):
+        row = []
+        for col in range(BOARDWIDTH):
+            row.append(counter)
+            counter += 1
+        board.append(row)
+
+    board[BOARDWIDTH-1][BOARDHEIGHT-1] = BLANK
+    return board
+
+
+def getBlankPosition(board): # lấy toạ độ điểm blank
+    for row in range(BOARDHEIGHT):
+        for col in range(BOARDWIDTH):
+            if board[row][col] == BLANK:
+                return (row, col)
+
+
+def makeMove(board, move):
+    # Di chuyển theo move được truyền vào
+    blanky, blankx = getBlankPosition(board)
+
+    if move == UP:
+        board[blanky][blankx], board[blanky + 1][blankx] = board[blanky + 1][blankx], board[blanky][blankx]
+    elif move == DOWN:
+        board[blanky][blankx], board[blanky - 1][blankx] = board[blanky - 1][blankx], board[blanky][blankx]
+    elif move == LEFT:
+        board[blanky][blankx], board[blanky][blankx + 1] = board[blanky][blankx + 1], board[blanky][blankx]
+    elif move == RIGHT:
+        board[blanky][blankx], board[blanky][blankx - 1] = board[blanky][blankx - 1], board[blanky][blankx]
+
+
+def isValidMove(board, move):
+    blanky, blankx = getBlankPosition(board)
+    return (move == UP and blanky != len(board) - 1) or \
+           (move == DOWN and blanky != 0) or \
+           (move == LEFT and blankx != len(board[0]) - 1) or \
+           (move == RIGHT and blankx != 0)
+
+
+def getRandomMove(board, lastMove=None):
+    # bắt đầu với đù 4 cách thức di chuyển
+    validMoves = [UP, DOWN, LEFT, RIGHT]
+
+    # xoá dần các cách thức chuyển không hợp lệ với board được truyền vào
+    if lastMove == UP or not isValidMove(board, DOWN): # Nếu bước di chuyển trước là UP thì bước di chuyển lần này không thể là DOWN hoặc board hiện tại không thể DOWN
+        validMoves.remove(DOWN)
+    if lastMove == DOWN or not isValidMove(board, UP): # Nếu bước di chuyển trước là DOWN thì bước di chuyển lần này không thể là UP hoặc board hiện tại không thể UP
+        validMoves.remove(UP)
+    if lastMove == LEFT or not isValidMove(board, RIGHT): # Nếu bước di chuyển trước là LEFT thì bước di chuyển lần này không thể là RIGHT hoặc board hiện tại không thể RIGHT
+        validMoves.remove(RIGHT)
+    if lastMove == RIGHT or not isValidMove(board, LEFT): # Nếu bước di chuyển trước là RIGHT thì bước di chuyển lần này không thể là LEFT hoặc board hiện tại không thể LEFT
+        validMoves.remove(LEFT)
+
+    # khi validMoves chỉ còn đúng các cách thức di chuyển hợp lệ thì chọn 1 trong số chúng
+    return random.choice(validMoves)
+
+
+def getLeftTopOfTile(tileX, tileY): # Nhận toạ độ ô trong board và trả về toạ độ của đỉnh trái phía trên của ô đó trên màn hình
+    left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
+    top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
+    return (left, top)
+
+
+def getSpotClicked(board, x, y): # Từ toạ độ pixel nhấn trên màn hình trả về toạ độ ô tương ứng trong board
+    for tileY in range(len(board)):
+        for tileX in range(len(board[0])):
+            left, top = getLeftTopOfTile(tileX, tileY)
+            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+            if tileRect.collidepoint(x, y): # kiểm tra toạ độ pixel truyền vào có nằm trong ô này không
+                return (tileX, tileY)
+    return (None, None)
+
+
+def drawTile(tilex, tiley, number, adjx=0, adjy=0): # vẽ ô từ toạ độ đỉnh trái phía trên của nó
+    left, top = getLeftTopOfTile(tilex, tiley)
+    pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left + adjx, top + adjy, TILESIZE, TILESIZE))
+    textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
+    textRect = textSurf.get_rect()
+    textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
+    DISPLAYSURF.blit(textSurf, textRect)
+
+
+def makeText(text, color, bgcolor, top, left): # thiết lập giao diện cho chữ
+    textSurf = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurf.get_rect()
+    textRect.topleft = (top, left)
+    return (textSurf, textRect)
+
+
+def drawBoard(board, message): # thiết lập giao diện hiển thị board và message và các text
+    DISPLAYSURF.fill(BGCOLOR)
+    if message:
+        textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
+        DISPLAYSURF.blit(textSurf, textRect)
+
+    for tiley in range(len(board)):
+        for tilex in range(len(board[0])):
+            if board[tiley][tilex]:
+                drawTile(tilex, tiley, board[tiley][tilex])
+
+    left, top = getLeftTopOfTile(0, 0)
+    width = BOARDWIDTH * TILESIZE
+    height = BOARDHEIGHT * TILESIZE
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+
+    DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
+    DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
+    DISPLAYSURF.blit(SOLVE_SURF, SOLVE_RECT)
+    DISPLAYSURF.blit(GUIDE_SURF, GUIDE_RECT)
+    DISPLAYSURF.blit(ABOUT_SURF, ABOUT_RECT)
+
+
+def slideAnimation(board, direction, message, animationSpeed): # hiển thị animation di chuyển ô
+
+    blanky, blankx = getBlankPosition(board)
+    if direction == UP:
+        movex = blankx
+        movey = blanky + 1
+    elif direction == DOWN:
+        movex = blankx
+        movey = blanky - 1
+    elif direction == LEFT:
+        movex = blankx + 1
+        movey = blanky
+    elif direction == RIGHT:
+        movex = blankx - 1
+        movey = blanky
+
+    # prepare the base surface
+    drawBoard(board, message)
+    baseSurf = DISPLAYSURF.copy()
+    # draw a blank space over the moving tile on the baseSurf Surface.
+    moveLeft, moveTop = getLeftTopOfTile(movex, movey)
+    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, TILESIZE, TILESIZE))
+
+    for i in range(0, TILESIZE, animationSpeed):
+        # animate the tile sliding over
+        checkForQuit()
+        DISPLAYSURF.blit(baseSurf, (0, 0))
+        if direction == UP:
+            drawTile(movex, movey, board[movey][movex], 0, -i)
+        if direction == DOWN:
+            drawTile(movex, movey, board[movey][movex], 0, i)
+        if direction == LEFT:
+            drawTile(movex, movey, board[movey][movex], -i, 0)
+        if direction == RIGHT:
+            drawTile(movex, movey, board[movey][movex], i, 0)
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def generateNewPuzzle(numSlides): # Tráo bảng ban đầu với numSlides lần
+    sequence = []
+    board = getStartingBoard()
+    drawBoard(board, '')
+    pygame.display.update()
+    pygame.time.wait(500) # đợi 500ms trước khi tráo
+    lastMove = None
+    for i in range(numSlides):
+        move = getRandomMove(board, lastMove)
+        slideAnimation(board, move, 'Generating new puzzle...', animationSpeed=int(TILESIZE / 3))
+        makeMove(board, move)
+        sequence.append(move)
+        lastMove = move
+    return (board, sequence)
+
+
+def resetAnimation(board, allMoves): # Đi ngược lại các cách thức di chuyển trong allMoves
+    revAllMoves = allMoves[:] # tạo một bản sao của allMoves
+    revAllMoves.reverse()
+
+    for move in revAllMoves:
+        if move == UP:
+            oppositeMove = DOWN
+        elif move == DOWN:
+            oppositeMove = UP
+        elif move == RIGHT:
+            oppositeMove = LEFT
+        elif move == LEFT:
+            oppositeMove = RIGHT
+        slideAnimation(board, oppositeMove, '', animationSpeed=int(TILESIZE / 2))
+        makeMove(board, oppositeMove)
+
+def showAboutDialog():
+    message = "Contributors:\n\nLương Vĩnh Lợi\nHoàng Đình Trung\nPhạm Duy Phú\nĐặng Hữu Tấn"     
+    messagebox.showinfo(title='About', message= message) 
+
+if __name__ == '__main__':
+    main()
--- src/a_star.py
+++ src/a_star.py
@@ -1,1 +1,164 @@
+from heapq import heapify, heappush, heappop
 
+#Tạo chuỗi dạng ma trận 2 chiều cho ma trận matrix được truyền vào
+def toString(matrix):
+    s = "\n"
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            s += str(matrix[i][j]) + " "
+        s += "\n"
+
+    return s
+
+#Tính khoảng cách manhattan của ma trận matrix được truyền vào
+def manhattan(matrix):
+    n = len(matrix)
+    counter = 0
+    expectedCoord = [0, 0]
+    for i in range (n):
+        for j in range (n):
+            value = matrix[i][j]
+            if (value == 0):
+                continue
+            expectedCoord[0] = (value - 1) // n
+            expectedCoord[1] = (value - 1) % n
+            counter += abs(expectedCoord[0] - i) + abs(expectedCoord[1] - j)
+    return counter
+
+#Kiểm tra ma trận matrix được truyền vào đã là ma trận đích chưa
+def isGoal(matrix):
+    temp = 0
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if (i == n - 1 and j == n - 1 and matrix[i][j] == 0):
+                break
+            temp += 1
+            if (matrix[i][j] != temp):
+                return False
+          
+    return True
+
+#Lấy các ma trận hàng xóm của ma trận current được truyền vào
+def  neighbors(current):
+    n = len(current[-1])
+    neighbors = []
+    blank = None# chứa toạ độ của điểm blank
+
+    #Lấy toạ độ điểm blank của ma trận trong biến current
+    for i in range(n):
+        try:
+            blank = current[i].index (0)
+        except Exception as e:
+            continue
+        blank = (i, blank)
+        break
+
+    if (blank[1] < (n - 1)):# Nếu có thể di chuyển blank sang phải
+        a = [ i.copy() for i in current ]
+        number = a[blank[0]][blank[1] + 1]
+        a[blank[0]][blank[1]], a[blank[0]][blank[1] + 1] = a[blank[0]][blank[1] + 1], a[blank[0]][blank[1]]
+        neighbors.append(('Click ' + str(number) + " or press the LEFT arrow key" + toString(a), a, (blank[0], blank[1] + 1)))
+
+    if (blank[1] > 0):# Nếu có thể di chuyển blank sang trái
+        b = [ i.copy () for i in current ]
+        number = b[blank[0]][blank[1] - 1]
+        b[blank[0]][blank[1]], b[blank[0]][blank[1] - 1] = b[blank[0]][blank[1] - 1], b[blank[0]][blank[1]]
+        neighbors.append(('Click ' + str(number) + " or press the RIGHT arrow key"+ toString(b), b, (blank[0], blank[1] - 1)))
+
+    if (blank[0] > 0):# Nếu có thể di chuyển blank đi lên
+        c = [ i.copy () for i in current ]
+        number = c[blank[0] - 1][blank[1]]
+        c[blank[0]][blank[1]], c[blank[0] - 1][blank[1]] = c[blank[0] - 1][blank[1]], c[blank[0]][blank[1]]
+        neighbors.append(('Click ' + str(number) + " or press the DOWN arrow key"+toString(c), c, (blank[0] - 1, blank[1])))
+
+    if (blank[0] < (n - 1)):# Nếu có thể di chuyển blank đi xuống
+        d = [ i.copy () for i in current ]
+        number = d[blank[0] + 1][blank[1]]
+        d[blank[0]][blank[1]], d[blank[0] + 1][blank[1]] = d[blank[0] + 1][blank[1]], d[blank[0]][blank[1]]
+        neighbors.append(('Click ' + str(number) + " or press the UP arrow key"+ toString(d), d, (blank[0] + 1, blank[1])))
+
+    return neighbors
+
+import time
+def solve(grid):# sử dụng thuật toán A* để giải
+    # biến currentNode có cấu trúc như sau:
+    # currentNode[0] độ ưu tiên
+    # currentNode[1] số bước đã dịch chuyển để tới ma trận này(ma trận lưu trong currentNode[3])
+    # currentNode[2] chứa các cách thức di chuyển
+    # currentNode[3] chứa ma trận hiện tại
+    currentNode = (manhattan(grid), 0, [], grid) # node đầu tiên
+    stateTree = [currentNode] # cây với 1 node đầu tiên
+    visited_matrix = [] # chứa danh sách các ma trận đã đi qua
+    inner_matrix = [currentNode[-1]] # chứa danh sách các ma trận đang ở trong stateTree
+
+    heapify(stateTree) # khởi tạo priority queue
+    start_time = time.time() # thời gian bắt đầu giải
+    while (not  isGoal(currentNode[-1])):
+        currentNode = heappop(stateTree) # lấy từ priority queue node có currentNode[0] nhỏ nhất
+        inner_matrix.remove(currentNode[-1]) # ma trận currentNode[-1] ko ở trong stateTree nữa nên xoá đi
+        visited_matrix.append(currentNode[-1]) # thêm vào những ma trận đã đi qua
+        # print(currentNode)
+
+        for state in  neighbors(currentNode[-1]): # lấy các ma trận hàng xóm
+            if (state[1] not in visited_matrix and state[1] not in inner_matrix): # Nếu ma trận state[1] đã đi qua hoặc ở trong pq rồi thì không thêm vào nữa
+                heappush(stateTree, 
+                    (
+                        manhattan(state[1]) + currentNode[1] + 1,
+                        currentNode[1] + 1, 
+                        currentNode[2] + [state[0]], 
+                        state[1]
+                    )
+                )
+                inner_matrix.append(state[1])
+
+    # Trả về số bước dịch chuyển, dãy các cách thức di chuyển, thời gian thực thi
+    return (currentNode[1], currentNode[2], round(time.time() - start_time, 2))
+
+from tkinter import *
+from tkinter import scrolledtext
+def guide(matrix):# cài đặt giao diện hiển thị kết quả
+    ws = Tk()
+    ws.title('Guide')
+    ws.geometry('400x640')
+    ws.configure(bg='#04bf87')
+    font_tuple = ("Helvetica", 12)
+    label = Label(ws, text = "Processing ...", font = font_tuple )
+    label.pack(expand=True)
+    ws.update()
+
+    report = ""
+    seqCount, sequence, time_excute = solve(matrix)
+    label.destroy()
+    report +=  str(time_excute) + " seconds\n"
+    report += str(seqCount) + " steps\n\n"
+    report += "Initial board:"
+    report += toString(matrix) + "\n"
+    report += '\n'.join (sequence)  
+
+    text_box = scrolledtext.ScrolledText(
+        ws,
+        height=140,
+        width=100,
+        wrap='word',
+        bg='#04bf87',
+        font=font_tuple
+    )
+    text_box.insert('end', report)
+    text_box.pack(side=LEFT,expand=True)
+    text_box.config(state='disabled')
+
+    ws.mainloop()
+    
+
+if (__name__ == '__main__'):
+    # 1.2 - 1.5s
+    # grid = [[1,4,0], [2,5,3], [7,6,8]]
+
+    # 7.2 - 8.5s
+    grid = [[6,5,4], [7,0,8], [1,2,3]]
+
+    # 0 - 0.01s
+    # grid = [[0,1,3], [4,2,5], [7,8,6]]
+    guide(grid)
